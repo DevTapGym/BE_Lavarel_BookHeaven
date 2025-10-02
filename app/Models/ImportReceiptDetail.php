e7165299a2a00e869dbd7e6c05a4aa5e54200fb7
DevTapGym <2001224389@huit.edu.vn>
2025-10-02 09:40:50 +0700

    add migrate  import product

---
+++ app/Models/ImportReceiptDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImportReceiptDetail extends Model
+{
+    //
+}
---
+++ database/migrations/2025_10_02_020801_create_import_receipt_details_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('import_receipt_details', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('quantity')->default(1);
+            $table->decimal('total_price', 15, 2)->default(0);
+
+            $table->foreignId('import_receipt_id')
+                ->constrained('import_receipts')
+                ->onDelete('cascade');
+
+            $table->foreignId('supply_id')
+                ->constrained('supplies');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('import_receipt_details');
+    }
+};
