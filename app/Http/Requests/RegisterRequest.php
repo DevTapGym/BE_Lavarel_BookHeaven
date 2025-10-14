fa63f588f3f22dfa0aa596ccc983f2a96dafc85e
DevTapGym <2001224389@huit.edu.vn>
2025-10-14 22:10:43 +0700

    fix auth to fit FE

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -26,6 +26,7 @@ class RegisterRequest extends FormRequest
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
+            'phone' => 'nullable|string|max:20',
             'password' => 'required|string|min:6|confirmed',
         ];
     }
