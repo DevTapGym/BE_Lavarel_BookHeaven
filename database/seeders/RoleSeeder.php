ae57f5d696586bdd512cdd334ba0a700a64761f8
Đặng Ngọc Tài <2001224231@hufi.edu.vn>
2025-10-29 14:12:08 +0700

    fix/permission

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -60,7 +60,7 @@ class RoleSeeder extends Seeder
             'update account',
             'update customer',
             'return order',
-            'view permissions by rolename',
+            'get permissions by role name',
         ])->get());
         $this->command->info('Roles have been seeded successfully!');
 
@@ -109,7 +109,7 @@ class RoleSeeder extends Seeder
 
             'complete import receipt',
             'return import receipt',
-            'view permissions by rolename',
+            'get permissions by role name',
         ])->get());
         $this->command->info('Roles have been seeded successfully!');
 
@@ -137,7 +137,7 @@ class RoleSeeder extends Seeder
             'create customer',
             'download order pdf',
             'create order for web',
-            'view permissions by rolename',
+            'get permissions by role name',
         ])->get());
         $this->command->info('Roles have been seeded successfully!');
     }
